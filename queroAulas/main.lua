a78b4033a68f9b2b9ff40e6de4891fa138eaf261
Mucio Jaziel <muciojaziel@hotmail.com>
2017-05-18 13:33:21 -0300

    Teste criação do módulo do usuário

--- queroAulas/main.lua
+++ queroAulas/main.lua
@@ -1,11 +1,12 @@
 -----------------------------------------------------------------------------------------
 --
--- main.lua
---
------------------------------------------------------------------------------------------
-
--- Your code here
-
---require
+local login = require("login")
+local p 	= require("professor")
+local sqlite3 	= require("sqlite3")
 
 
+p.nome = "mucio"
+p.formacao = "sup"
+print("Nome do Fêssor "..p.nome.." com formacao "..p.formacao)
+
+print("versao ", sqlite3.version())
---
+++ queroAulas/professor.lua
@@ -0,0 +1,11 @@
+
+
+professor = {nome, formacao, especialidade}
+
+return professor
+
+--print(user.nome)
+
+--function buscar()
+	
+--end
