105b5c0d8e4e061e91d2bd55c6cb9397b5e1ec43
Mucio Jaziel <muciojaziel@hotmail.com>
2017-05-18 17:06:03 -0300

    Teste com BD

--- queroAulas/main.lua
+++ queroAulas/main.lua
@@ -1,12 +1,31 @@
------------------------------------------------------------------------------------------
---
-local login = require("login")
-local p 	= require("professor")
-local sqlite3 	= require("sqlite3")
+local sqlite3 = require( "sqlite3" )
+ 
+-- Open "data.db". If the file doesn't exist, it will be created
+local path = system.pathForFile( "teste.db", system.DocumentsDirectory )
+local dbt = sqlite3.open( path )   
+
+local tableSet = [[CREATE TABLE IF NOT EXISTS professor (id INTEGER PRIMARY KEY, nome, formacao);]]
+print( tableSet )
+dbt:exec( tableSet )
+
+local profDados = {}
+
+profDados.nome1 =  "Sofi"
+profDados.formacao1 = "Eng. Agrimensura"
 
 
-p.nome = "mucio"
-p.formacao = "sup"
-print("Nome do Fêssor "..p.nome.." com formacao "..p.formacao)
+local insrt1 = [[INSERT INTO professor VALUES (NULL, ']]..profDados.nome1..[[',']]..profDados.formacao1..[['); ]]
+--local insrt2 = [[INSERT INTO professor VALUES (NULL, ']]..profDados.nome2..[[',']]..profDados.formacao2..[['); ]]
 
-print("versao ", sqlite3.version())
+
+--dbt:exec(insrt1)
+--db:exec(insrt2)
+
+for row in dbt:nrows("SELECT * FROM professor") do
+    local textNome = "Nome individuo: "..row.nome.."".." - formacao dele: "..row.formacao
+    --local textFormac = row.formacao
+    local t = display.newText(textNome, 150, 30*row.id, nil, 16)
+    t:setFillColor( 1, 0, 1 )
+    print(row.id ,row.nome)
+    print(row.id, row.formacao)
+end
