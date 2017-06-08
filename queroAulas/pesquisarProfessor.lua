79b98fec18fbf3312d3f4b67bb463d33e6c33c33
Mucio Jaziel <muciojaziel@hotmail.com>
2017-06-07 23:53:09 -0300

    Modificado BD para array1

--- queroAulas/pesquisarProfessor.lua
+++ queroAulas/pesquisarProfessor.lua
@@ -44,7 +44,11 @@ function scene:hide( event )
  
     end
 end
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> d16edbe... Modificado BD para array
 -------require-----------------
 local widget = require ("widget")
 
@@ -150,6 +154,10 @@ function pesquisarProfessorNome( event )
 
 		tableView:insertRow({
 			rowHeight = 200,
+
+		tableView:insertRow({
+			rowHeight = 200,
+
 		})
 	end	
 
